66ff398f2082f319d3d478d9142948eb70f6c386
Nikolaj Kristensen <nikoak2@live.dk>
2023-09-08 20:04:50 +0200

    Changed default layout

---
+++ src/components/event-card.tsx
@@ -0,0 +1,55 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Calendar, Pencil, Trash, UserPlus } from "lucide-react";
+
+type EventCardProps = {
+  title: string;
+  description: string;
+  startTime: Date;
+  endTime: Date;
+  rsvp: Date;
+};
+
+export const EventCard = ({
+  title,
+  startTime,
+  endTime,
+  rsvp,
+}: EventCardProps) => {
+  return (
+    <div className="flex items-center border-2 rounded-lg p-4">
+      <Avatar className="h-9 w-9">
+        <AvatarImage src="/avatars/01.png" alt="Avatar" />
+        <AvatarFallback>OM</AvatarFallback>
+      </Avatar>
+      <div className="ml-4 space-y-1">
+        <p className="text-sm font-medium leading-none">{title}</p>
+        <p className="flex space-x-2 text-sm text-muted-foreground">
+          <span className="flex items-center">
+            <Calendar className="mr-2 h-4 w-4" />
+            {startTime.toLocaleDateString()}
+          </span>
+          <span className="flex items-center">
+            <Calendar className="mr-2 h-4 w-4" />
+            {endTime.toLocaleDateString()}
+          </span>
+          <span className="flex items-center">
+            <Calendar className="mr-2 h-4 w-4" />
+            {rsvp.toLocaleDateString()}
+          </span>
+        </p>
+      </div>
+      <div className="ml-auto space-x-2">
+        <Button variant="ghost" size="icon">
+          <UserPlus />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Pencil />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Trash />
+        </Button>
+      </div>
+    </div>
+  );
+};
--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -2,8 +2,11 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 
 export const UserAvatar = () => {
   return (
-    <Avatar className="h-12 w-12">
-      <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+    <Avatar className="h-9 w-9">
+      <AvatarImage
+        src="https://avatars.githubusercontent.com/u/124599?v=4"
+        alt="@shadcn"
+      />
       <AvatarFallback>CN</AvatarFallback>
     </Avatar>
   );
